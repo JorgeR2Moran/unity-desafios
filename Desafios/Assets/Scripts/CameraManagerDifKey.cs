64f0a7172623983e6234dadf2809f53044a667b9
JorgeR2Moran <jorge.arturo.mb@gmail.com>
2022-08-01 18:15:19 -0500

    Commit Desafio clase 07
    Commit Desafío clase 07.
    
    Modificación de manejo de teclas al cambiar cámara.

---
+++ Desafios/Assets/Scripts/CameraManagerDifKey.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagerDifKey : MonoBehaviour
+{
+    public GameObject[] cameras;
+    private Dictionary<KeyCode, GameObject> cameraList =  new Dictionary<KeyCode, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraList.Add(KeyCode.F1, cameras[0]);
+        cameraList.Add(KeyCode.F2, cameras[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(KeyValuePair<KeyCode, GameObject> camera in cameraList)
+        {
+            if(Input.GetKeyDown(camera.Key))
+            {
+                camera.Value.SetActive(true);
+                foreach(KeyValuePair<KeyCode, GameObject> disableCamera in cameraList)
+                {
+                    if(disableCamera.Value != camera.Value)
+                    {
+                        disableCamera.Value.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
